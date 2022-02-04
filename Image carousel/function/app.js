c6946bc3780a616f3b4872afb049ed08acc643df
tressyjobish <tressyjobish@gmail.com>
2022-02-04 16:26:30 +0530

    3d image

---
+++ Image carousel/function/app.js
@@ -0,0 +1,13 @@
+// function myFunction(val){
+//     console.log('Hello world',val);
+// }
+
+// const values=[1,2,3,4,5,6,7];
+// values.forEach(myFunction);
+
+// Arrow function
+
+// 
+// 
+
+function add()
